af28752264218d05c706a22f91569a5690330d0e
Antonio Navas <antonioluis_7_@hotmail.com>
2023-07-18 18:45:26 +0200

    "done"

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,30 +1,53 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => {
+    this.currentTime ++;
+    
+    if(printTimeCallback){
+      printTimeCallback();
+    }
+    },1000)
   }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime / 60)
   }
 
   getSeconds() {
     // ... your code goes here
+    let seconds = this.currentTime
+    return Math.floor(seconds % 60)
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    let valuetostring = value.toString();
+
+    if (valuetostring.length === 1){
+      return `0${valuetostring}`;
+
+    } else{
+      
+      return valuetostring;
+    }
   }
 
   stop() {
     // ... your code goes here
+    this.intervalId = clearInterval (this.intervalId);
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
